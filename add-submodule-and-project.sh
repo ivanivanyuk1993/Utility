834da470b827d866901aac4d270dd9c49e9b0d2f
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2021-12-31 13:36:06 +0300

    `CliExitCodeProviderNS` Added `CliExitCodeProviderNS`

---
+++ add-submodule-and-project.sh
@@ -0,0 +1,10 @@
+# Writing first argument to named variable to increase readability
+REPOSITORY_NAME=$1
+
+git submodule add \
+  "git@github.com:ivanivanyuk1993/UtilDotnet.${REPOSITORY_NAME}.git" \
+  "${REPOSITORY_NAME}" \
+&& \
+dotnet sln UtilDotnet.sln add \
+  "${REPOSITORY_NAME}/${REPOSITORY_NAME}NS/${REPOSITORY_NAME}NS.csproj" \
+  --solution-folder "${REPOSITORY_NAME}"
