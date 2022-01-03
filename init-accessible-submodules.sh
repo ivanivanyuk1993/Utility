8e6e06bd0eda070258cb9e981c77ea84e4676119
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-01-03 23:30:40 +0300

    Updated `RestrictedGitSubmoduleCliUtil` and added run of `MakeSolutionWithAccessibleCsprojectsCli` to `init-accessible-submodules.sh`

--- init-accessible-submodules.sh
+++ init-accessible-submodules.sh
@@ -4,4 +4,10 @@ dotnet run \
     --project RestrictedGitSubmoduleCliUtil/GitSubmoduleInitAccessibleCli/GitSubmoduleInitAccessibleCli.csproj \
     --configuration Release \
     -- \
-    --git-root-directory-path "."
+    --git-root-directory-path "." \
+&& \
+dotnet run \
+    --project RestrictedGitSubmoduleCliUtil/MakeSolutionWithAccessibleCsprojectsCli/MakeSolutionWithAccessibleCsprojectsCli.csproj \
+    --configuration Release \
+    -- \
+    --solution-file-path "./UtilDotnet.sln"
