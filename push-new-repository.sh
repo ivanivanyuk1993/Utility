619a79e5cec860b666c59087a8dbb635b4b660f3
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-01-08 20:28:02 +0300

    Removed unneeded scripts

--- push-new-repository.sh
+++
@@ -1,6 +0,0 @@
-# Writing first argument to named variable to increase readability
-REPOSITORY_NAME=$1
-
-git remote add origin "git@github.com:ivanivanyuk1993/UtilDotnet.${REPOSITORY_NAME}.git"
-git branch -M main
-git push -u origin main
